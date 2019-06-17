8dc3794ab5a93399e6f1532ff69b06565ae31714
zzyy12 <43592960+zzyy12@users.noreply.github.com>
2019-06-17 15:13:12 +0800

    617-6

--- main.ts
+++ main.ts
@@ -517,8 +517,8 @@ namespace HaodaBit {
         basic.pause(50);
         TCS34725_LOCK();
         let sum = clear;
-        let r = red / 5;
-        //r /= sum;
+        let r = red ;
+        r /= sum;
         let g = green / sum;
         //g /= sum;
         let b = blue;
@@ -527,7 +527,7 @@ namespace HaodaBit {
         g *= 256;
         b *= 256;
         if (a == 0) {
-            return clear;
+            return r;
         } else if (a == 1) {
             return g;
         } else if (a == 2) {
