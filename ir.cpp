a9655ac54478b28f7ce570520da8382c071ce98f
zzyy12 <43592960+zzyy12@users.noreply.github.com>
2019-06-20 16:58:52 +0800

    irtest11

--- ir.cpp
+++ ir.cpp
@@ -9,13 +9,25 @@ enum class Pins{
   P0=  3,
   P1=  2,
   P2=  1,
+  P3=  4,
+  P4=  5,
+  P5=  17,
+  P6=  12,
+  P7=  11,
   P8=  18,
+  P9=  10,
+  P10= 6,
+  P11= 26,
   P12= 20,
-  P16= 16
+  P13= 23,
+  P14= 22,
+  P15= 21,
+  P16= 16,
+  P19= 0,
+  P20= 30
 };
 
 enum class RemoteButton {
-	
         IR_BUTTON_0 = 0x4f,
         IR_BUTTON_1 = 0xff,
         IR_BUTTON_2 = 0x7f,
@@ -33,8 +45,6 @@ enum class RemoteButton {
         IR_BUTTON_RIGHT = 0x97,
         IR_BUTTON_SPARK = 0xcf,
         IR_BUTTON_POUND = 0x8f
-    
-	
 };
 
 //% color=50 weight=19
