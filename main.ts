d202ee784989dc8966669b2b77bb47b1de906bfa
zzyy12 <43592960+zzyy12@users.noreply.github.com>
2019-06-20 15:03:25 +0800

    irtest5

--- main.ts
+++ main.ts
@@ -49,6 +49,34 @@ namespace HaodaBit {
     const TCS34725Gain = 0x01
     const TCS34725_COMMAND_BIT = 0x80
     const TCS34725_ADDRESS = 0x29
+	
+	// Auto-generated. Do not edit.
+
+
+
+
+
+     enum RemoteButton {   
+	
+        IR_BUTTON_0 = 0x4f,
+        IR_BUTTON_1 = 0xff,
+        IR_BUTTON_2 = 0x7f,
+        IR_BUTTON_3 = 0xbf,
+        IR_BUTTON_4 = 0xdf,
+        IR_BUTTON_5 = 0x5f,
+        IR_BUTTON_6 = 0x9f,
+        IR_BUTTON_7 = 0xef,
+        IR_BUTTON_8 = 0x6f,
+        IR_BUTTON_9 = 0xaf,
+        IR_BUTTON_OK = 0x57,
+        IR_BUTTON_UP = 0x77,
+        IR_BUTTON_DOWN = 0x67,
+        IR_BUTTON_LEFT = 0xd7,
+        IR_BUTTON_RIGHT = 0x97,
+        IR_BUTTON_SPARK = 0xcf,
+        IR_BUTTON_POUND = 0x8f
+    
+    }
 
 
 
@@ -558,7 +586,8 @@ namespace HaodaBit {
     //% blockId=HaodaBit_ir_init block="连接红外接收在 %pin"
     //% weight=100
 	//% shim=Mbit_IR::init
-   export function init(pin: Pins): void{
+   export function init(pin: Ports): void{
+	  let pindd = PortDigital[port]
 	   
    }
 }
