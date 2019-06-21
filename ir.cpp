20c230a799f7b530aeec5feb76cff434ce1dc8b9
zzyy12 <43592960+zzyy12@users.noreply.github.com>
2019-06-21 13:57:27 +0800

    fix

--- ir.cpp
+++ ir.cpp
@@ -15,23 +15,23 @@ enum class Pins{
 };
 
 enum class RemoteButton {
-        IR_BUTTON_0 = 0x4f,
-        IR_BUTTON_1 = 0xff,
-        IR_BUTTON_2 = 0x7f,
-        IR_BUTTON_3 = 0xbf,
-        IR_BUTTON_4 = 0xdf,
-        IR_BUTTON_5 = 0x5f,
-        IR_BUTTON_6 = 0x9f,
-        IR_BUTTON_7 = 0xef,
-        IR_BUTTON_8 = 0x6f,
-        IR_BUTTON_9 = 0xaf,
-        IR_BUTTON_OK = 0x57,
-        IR_BUTTON_UP = 0x77,
-        IR_BUTTON_DOWN = 0x67,
-        IR_BUTTON_LEFT = 0xd7,
-        IR_BUTTON_RIGHT = 0x97,
-        IR_BUTTON_SPARK = 0xcf,
-        IR_BUTTON_POUND = 0x8f
+        IR_BUTTON_0 = 0x0d,
+        IR_BUTTON_1 = 0x00,
+        IR_BUTTON_2 = 0x01,
+        IR_BUTTON_3 = 0x02,
+        IR_BUTTON_4 = 0x04,
+        IR_BUTTON_5 = 0x05,
+        IR_BUTTON_6 = 0x06,
+        IR_BUTTON_7 = 0x08,
+        IR_BUTTON_8 = 0x09,
+        IR_BUTTON_9 = 0x0a,
+        IR_BUTTON_OK = 0x15,
+        IR_BUTTON_UP = 0x11,
+        IR_BUTTON_DOWN = 0x19,
+        IR_BUTTON_LEFT = 0x14,
+        IR_BUTTON_RIGHT = 0x16,
+        IR_BUTTON_SPARK = 0x0c,
+        IR_BUTTON_POUND = 0x0e
 };
 
 //% color=50 weight=80
